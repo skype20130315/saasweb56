406c2d504ae69957eaa8d0ec3ab04ffc98c86a16
bfuture <272891405@qq.com>
2013-04-10 11:31:14 +0800

    Signed-off-by: bfuture <272891405@qq.com>

--- src/com/bfuture/app/saas/service/TCDocQueryManager.java
+++ src/com/bfuture/app/saas/service/TCDocQueryManager.java
@@ -8,8 +8,5 @@ import com.bfuture.app.basic.service.BaseManager;
  *
  */
 public interface TCDocQueryManager extends BaseManager {
-	/**为了打印退厂单*/
-	public ReturnObject getTCDocDetail(Object[] o);//获得列表
-	public ReturnObject getHead(Object[] o);//合计
-	public ReturnObject searchIns(String sgcode);//实例中文名
+
 }
